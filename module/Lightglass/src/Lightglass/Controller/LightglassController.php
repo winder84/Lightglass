b7f07510e5f7542a79fbf740c8868f76e1e6dd75
winder84 <winder84@mail.ru>
2013-04-10 23:54:09 +0400

    всё кроме админки

--- module/Lightglass/src/Lightglass/Controller/LightglassController.php
+++ module/Lightglass/src/Lightglass/Controller/LightglassController.php
@@ -32,6 +32,38 @@ class LightglassController extends AbstractActionController
 
 	public function projectAction()
 	{
+		$id = (int) $this->params()->fromRoute('id', 1);
+
+		switch ($id) {
+			case 1:
+				$title = 'Волгодонска атомная электростанция';
+				$desc = 'Был произведён комплекс работ по реконструкции здания АБК Волгодонской АЭС.
+				Были установлены алюминиевые фасадные конструкции с применением безопасного остекления.
+				Входные группы выполнены с помощью автоматических систем GEZE (Германия)';
+				$img = array('volg_ae.jpg');
+				break;
+			case 2:
+				$title = 'Marc Cain';
+				$desc = 'Входная группа из закалённого стекла.';
+				$img = array('marccain_f.jpg');
+				break;
+			case 3:
+				$title = 'Банк "Восточный Экспресс"';
+				$desc = 'Алюминиевые конструкции, внутренние перегородки из закаленного стекла, нанесение рисунка с помощью пескоструйной обработки.';
+				$img = array('bank_1.jpg', 'bank_2.jpg', 'bank_3.jpg');
+				break;
+			case 4:
+				$title = 'Пиццерия & Кафе Сан-Ремо';
+				$desc = '';
+				$img = array('sanremo.jpg');
+				break;
+		}
+
+		return new ViewModel(array(
+			'title' => $title,
+			'desc' => $desc,
+			'img' => $img,
+		));
 	}
 
 	public function addAction()
--- public/js/lightglass.js
+++ public/js/lightglass.js
@@ -6,6 +6,8 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var main_par_index = 1;
+
 $(document).ready(function() {
 
 });
@@ -25,4 +27,29 @@ function tech_b(id) {
 				$('#tech_text_'+id).fadeIn(100)})})})
 	$('.current').removeClass('current');
 	$('#t_b_'+id).attr('class','current');
+}
+
+function main_par_b(i) {
+	$('#par_logo_'+main_par_index).slideUp(100);
+	$('#par_id_'+main_par_index).slideUp(100);
+
+	switch (i) {
+		case 1:
+			main_par_index--;
+			break;
+		case 2:
+			main_par_index++;
+			break;
+	}
+
+	if(main_par_index == 0) {
+		main_par_index = 4
+	} else {
+		if(main_par_index == 5) {
+			main_par_index = 1
+		}
+	};
+	$('#par_logo_'+main_par_index).slideDown(100);
+	$('#par_id_'+main_par_index).slideDown(100);
+
 }
